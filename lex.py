6c65e607d0a9b5c0ff5f4387a55b56bb8362149e
jhossias <jhopacal@espol.edu.ec>
2021-11-14 11:29:15 -0500

    Analizador lexico, se coloca ejemplo y cambios previos

---
+++ lex.py
@@ -0,0 +1,58 @@
+import ply.lex as lex
+
+# List of token names.   This is always required
+tokens = (
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'LPAREN',
+    'RPAREN',
+)
+
+# Regular expression rules for simple tokens
+t_PLUS    = r'\+'
+t_MINUS   = r'-'
+t_TIMES   = r'\*'
+t_DIVIDE  = r'/'
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+
+#Jhossias Calderon
+# Expresión para considerar que un numero es de tipo entero
+def t_NUMBER(t):
+    r'\d+|\-\d+'
+    t.value = int(t.value)
+    return t
+
+#Expresión para considerar que un numero es de tipo float
+def t_FLOAT(t):
+    r'\d+\.\d+|\-\d+\.\d+'
+    t.value = float(t.value)
+    return t
+
+# Define a rule so we can track line numbers
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+# A string containing ignored characters (spaces and tabs)
+t_ignore  = ' \t'
+
+# Error handling rule
+def t_error(t):
+    print("Componente léxico no reconocido '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+def getTokens(lexer):
+    while True:
+        tok = lexer.token()
+        if not tok:
+            break  # No more input
+        print(tok)
+
+
+# Build the lexer
+lexer = lex.lex()
+
