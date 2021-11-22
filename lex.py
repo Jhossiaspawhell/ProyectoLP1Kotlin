dc0e27cbb52b9e90b71bbfe32eddbcc56d2e550b
Gustavo Chonillo <gustavo.chonillo@gmail.com>
2021-11-22 01:23:52 -0500

    reparación a nivel general

--- lex.py
+++ lex.py
@@ -2,39 +2,41 @@ from typing import DefaultDict
 import ply.lex as lex
 
 #Autor: Jhossias Calderon, Gustavo Chonillo
-#Ingreso de palabras reservadas
-reserved = {
-    'as' : 'AS', #'as?' : 'AS?',
-    'break' : 'BREAK', 'class' : 'CLASS',
-    'continue' : 'CONTINUE', 'do' : 'DO',
-    'else' : 'ELSE', 'false' : 'FALSE', 'for' : 'FOR',
-    'fun' : 'FUN', 'if' : 'IF', 'in' : 'IN', #'!in' : '!IN',
-    'interface' : 'INTERFACE', 'is' : 'IS',
-    #'!is' : '!IS',
-    'null' : 'NULL', 'object' : 'OBJECT',
-    'package' : 'PACKAGE',
-    'return' : 'RETURN', 'super' : 'SUPER', 'this' : 'THIS',
-    'throw' : 'THROW',
-    'true' : 'TRUE', 'try' : 'TRY',
-    'typealias' : 'TYPEALIAS', 'typeof' : 'TYPEOF', 'val' : 'VAL', 'var' : 'VAR',
-    'when' : 'WHEN', 'while' : 'WHILE',
-    'by' : 'BY',
-    'catch' : 'CATCH', 'constructor' : 'CONSTRUCTOR', 'delegate' : 'DELEGATE',
-    'dynamic' : 'DYNAMIC', 'field' : 'FIELD',
-    'file' : 'FILE',
-    'finally' : 'FINALLY', 'get' : 'GET',
-    'import' : 'IMPORT', 'init' : 'INIT',
-    'param' : 'PARAM', 'property' : 'PROPERTY',
-    'receiver' : 'RECEIVER', 'set' : 'SET',
-    'setparam' : 'SETPARAM', 'value' : 'VALUE',
-    'where' : 'WHERE', 'and' : 'AND', 'or' : 'OR'
-}
+textoSalida = "" #Texto en el que se escribe la salida
 
-
-# List of token names.   This is always required
+reserved ={
+    'if ':'IF',
+    'then': 'THEN',
+    'else' :'ELSE',
+    'while' :'WHILE',
+    'for' : 'FOR',
+    'var':'VAR',
+    'val':'VAL',
+    'int':'INT',
+    'String':'STRING',
+    'boolean':'BOOLEAN',
+    'var':'VAR',
+    'val':'VAL',
+    'fun':'FUN',
+    'size': 'SIZE',
+    'setof':'SETOF',
+    'set':'SET',
+    'listof':'LISTOF',
+    'list':'LIST',
+    'or' :'OR',
+    'and':'AND',
+    'in' : 'IN',
+    'rindex': 'RINDEX',
+    'contains':'CONTAINS',
+    'readline':'READLINE',
+    'print':'PRINT',
+    'println':'PRINTLN',
+    'return':'RETURN',
+    }
 tokens = (
     'NUMBER',
-    'FLOAT',
+    'STRINGPALABRA',
+    'BOOLEANPALABRA',
     'PLUS',
     'MINUS',
     'TIMES',
@@ -45,125 +47,80 @@ tokens = (
     'RLLAVE',
     'MAYORQUE',
     'MENORQUE',
+    'VARIABLE',
     'IGUAL',
     'MOD',
-    'POW',
-    # Gustavo Chonillo
-    'PUNTO',
-    'COMA',
     'PUNTOYCOMA',
     'DOSPUNTOS',
     'ESIGUAL',
+    'COMA',
+    'PUNTO',
     'DIFERENTE',
-    'NEGACION',
     'LCLASP',
     'RCLASP',
-    'INCREMENT',
-    'DECREMENT',
-    #Variables
-    'VARIABLE',
-) + tuple(reserved.values())
-
+    'SALTOLINEA'
+ ) + tuple(reserved.values())
 # Regular expression rules for simple tokens
-t_PLUS     = r'\+'
-t_MINUS    = r'-'
-t_TIMES    = r'\*'
-t_DIVIDE   = r'/'
-t_LPAREN   = r'\('
-t_RPAREN   = r'\)'
-t_LLLAVE   = r'{'
-t_RLLAVE   = r'}'
-#Gustavo Chonillo
-t_MAYORQUE = r'>'
-t_MENORQUE = r'<'
-t_IGUAL    = r'='
-t_MOD      = r'%'
-t_PUNTO    = r'\.'
-t_COMA     = r','
+t_SALTOLINEA=r'(\\n)'
+t_PLUS = r'\+'
+t_MINUS = r'-'
+t_TIMES = r'\*'
+t_DIVIDE = r'/'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+t_MAYORQUE =  r'>'
+t_MENORQUE =  r'<'
+t_MOD = r'%'
+t_IGUAL= r'='
+t_PUNTO=r'\.'
 t_PUNTOYCOMA=r';'
-t_DOSPUNTOS= r':'
-t_ESIGUAL  = r'=='
-t_DIFERENTE= r'!='
-t_NEGACION = r'!'
-t_LCLASP   = r'\['
-t_RCLASP   = r'\]'
-t_INCREMENT= r'\+\+'
-t_DECREMENT= r'--'
-# RESERVADOS Gustavo Chonillo
-t_AS       = r'(as)'
-t_BREAK    = r'(break)'
-t_CLASS    = r'(class)'
-t_CONTINUE = r'(continue)'
-t_DO       = r'(do)'
-t_ELSE     = r'(else)'
-t_FALSE    = r'(false)'
-t_FOR      = r'(for)'
-t_FUN      = r'(fun)'
-t_IF       = r'(if)'
-t_IN       = r'(in)'
-t_INTERFACE= r'(interface)'
-t_IS       = r'(is)'
-t_NULL     = r'(null)'
-t_OBJECT   = r'(object)'
-t_PACKAGE  = r'(package)'
-t_RETURN   = r'(return)'
-t_SUPER    = r'(super)'
-t_THIS     = r'(this)'
-t_THROW    = r'(throw)'
-t_TRUE     = r'(true)'
-t_TRY      = r'(try)'
-t_TYPEALIAS= r'(typealias)'
-t_TYPEOF   = r'(typeof)'
-t_VAL      = r'(val)'
-t_VAR      = r'(var)'
-t_WHEN     = r'(when)'
-t_WHILE    = r'(while)'
-t_BY       = r'(by)'
-t_CATCH    = r'(catch)'
-t_CONSTRUCTOR= r'(constructor)'
-t_DELEGATE = r'(delegate)'
-t_DYNAMIC  = r'(dynamic)'
-t_FIELD    = r'(field)'
-t_FILE     = r'(file)'
-t_FINALLY  = r'(finally)'
-t_GET      = r'(get)'
-t_IMPORT   = r'(import)'
-t_INIT     = r'(init)'
-t_PARAM    = r'(param)'
-t_PROPERTY = r'(property)'
-t_RECEIVER = r'(receiver)'
-t_SET      = r'(set)'
-t_SETPARAM = r'(setparam)'
-t_VALUE    = r'(value)'
-t_WHERE    = r'(where)'
-t_AND      = r'(and)'
-t_OR       = r'(or)'
-
-#Jhossias Calderon
-#Variables
-def t_VARIABLE(t): 
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reserved.get(t.value, 'VARIABLE')  # Check for reserved words
-    return t
+t_DOSPUNTOS=r':'
+t_ESIGUAL=r'=='
+t_COMA=r','
+t_DIFERENTE=r'!='
+t_LLLAVE=r'{'
+t_RLLAVE=r'}'
+t_LCLASP=r'\['
+t_RCLASP=r'\]'
 
 
-#identificador
-def t_IDENTIFIER(t):
-    r'[a-zA-Z_][a-zA-Z0-9_]*'
-    t.type = reserved.get(t.value, 'IDENTIFIER')  # Check for reserved words
-    return t
+# RESERVADOS
+t_RETURN=r'(return)'
+t_PRINT=r'(print)'
+t_PRINTLN=r'(println)'
+t_READLINE=r'(readLine\(\))'
+t_INT=r'(Int) | (int) | (INT)'
+t_ELSE =r'(else)'
+t_IF=r'(if)'
+t_THEN=r'(then)'
+t_FOR = r'(for)'
+t_AND=r'(and)'
+t_OR= r'(or)'
+t_VAR=r'(var)'
+t_VAL=r'(val)'
+t_STRING=r'(String)'
+t_BOOLEAN=r'(Boolean)'
+t_FUN=r'(fun)'
+t_SIZE=r'(size\(\))'
+t_RINDEX=r'(rindex)'
+t_CONTAINS=r'(contains)'
+t_WHILE=r'(while)'
+t_SETOF=r'(setOf)'
+t_LISTOF=r'(listOf)'
+t_LIST=r'(List)'
+t_SET=r'(Set)'
+t_BOOLEANPALABRA=r'(True) | (False) '
+t_IN = r'(in)'
 
-#Jhossias Calderon
-# Expresión para considerar que un numero es de tipo entero
+
+# VARIABLE
+t_VARIABLE= r'\w+'
+# A regular expression rule with some action code
+
+
 def t_NUMBER(t):
-    r'\d+|\-\d+'
+    r'\d+'
     t.value = int(t.value)
-    return t
-
-#Expresión para considerar que un numero es de tipo float
-def t_FLOAT(t):
-    r'\d+\.\d+|\-\d+\.\d+'
-    t.value = float(t.value)
     return t
 
 # Define a rule so we can track line numbers
@@ -172,43 +129,18 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 # A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+t_ignore = ' \t'
 
 # Error handling rule
 def t_error(t):
-    print("Componente léxico no reconocido '%s'" % t.value[0])
+    global textoSalida
+    textoSalida +="No es reconocido '%s'" % t.value[0]+"\n"
     t.lexer.skip(1)
 
-def getTokens(lexer):
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break  # No more input
-        print(tok)
-
-
+def t_STRINGPALABRA(t):
+    r'"[a-zA-Z0-9\s]*" |\'[a-zA-Z0-9\s]*\''
+    t.value = str(t.value)
+    return t
 # Build the lexer
 lexer = lex.lex()
-
 # Test it out
-data = '''
- 3 + 4 * 10
-   + -20 *2 + ab
-  while (true){
-    a++
-  }
-  if (a and b){
-    return 20
-  }
-  if (a in c)
-'''
-
-# Give the lexer some input
-lexer.input(data)
-
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok)
