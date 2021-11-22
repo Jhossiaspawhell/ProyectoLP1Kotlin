46d353b6d509106e99d35a4d424863655e796edf
jhossias <jhopacal@espol.edu.ec>
2021-11-21 20:49:40 -0500

    Inicio analizador sintactico

---
+++ sintactico.py
@@ -0,0 +1,8 @@
+mport ply.yacc as yacc
+# import lexicoProyecto
+
+# Get the token map from the lexer.  This is required.
+from lexicoProyecto import tokens
+from lexicoProyecto import reserverd
+
+textoSalida = "" #Texto en el que se escribe la salida
